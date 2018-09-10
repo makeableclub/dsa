57348e3f40841992377bee9fcc3d1e66426ae760
Xie <yxie1@MTVL1638015f6.local>
2018-09-09 18:37:09 -0700

    optimization code

--- climb-stairs/climb-array.js
+++ climb-stairs/climb-array.js
@@ -2,26 +2,34 @@
 // return: the array of actual unique steps
 
 var answers = [];
-answers[0] = 1;
-answers[1] = 1;
-answers[2] = 2;
+answers[0] = [""];
+answers[1] = ["1"];
+answers[2] = ["1 2", "- 2"];
 
 function climb(n) {
 	// boundary condition
 	if( n==1 ) return [1];
 	if( n==2 ) return ["1 2", "- 2"];
 
-	var ways = [];
+	// look into cache first
+	if( answers[n] !== undefined && answers[n].length > 0) {
+		console.log("cache hit for " + n);
+		return answers[n];
+	}
 
+	// if cache miss, you have to do recursion call below
+	var ways = [];
 	var prev = climb(n-1);
 	for(var i=0; i<prev.length; i++) {
 		ways.push(prev[i] + " " + n);
 	}
-
 	var pprev = climb(n-2);
 	for(var i=0; i<pprev.length; i++) {
 		ways.push(pprev[i] + " - " + n);
 	}
+
+	// save the result into cache, "ways" is the result
+	answers[n] = ways;
 
 	return ways;
 }
--- climb-stairs/climb-re.js
+++ climb-stairs/climb-re.js
@@ -1,41 +1,38 @@
 // function to get the number of ways of climbing N steps, with max leap of 2
 // return: count
 
+// as cache to save the "ways"  for each N
 var answers = [];
 answers[0] = 1;
 answers[1] = 1;
 answers[2] = 2;
 
 function climb(n) {
-	// boundary condition
-	// hh	if( n <= 2 ) return n;
+	// base/boundary condition
+	// if( n <= 2 ) return n;
 	if( n==1 ) {
-		console.log(n);
 		return 1;
 	}
 	if( n==2 ) {
-		console.log(n);
 		return 2;
 	}
 	// look up for a value
-	if( answers[n] !== undefined && answers[n] > 1 )
+	if( answers[n] !== undefined && answers[n] > 1 ) {
+		console.log("cache hit");
 		return answers[n];
+	}
 
 	// console.log("before" + n);
-
+	// recursive call to find answer for "n"
 	var sum = climb(n-1) + climb(n-2);
-
-	console.log("after" + n);
 
 	// save the "sum" to an array for future use;
 	answers[n] = sum;
-
-
-
+	console.log("save to cache: " + n + ":" + sum);
 	return sum;
 }
 
 
-let count = climb(5);
+let count = climb(14);
 // let count = climb(354);
 console.log( "total ways: " + count);
